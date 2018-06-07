c81b39eaee220a619af561675ad6c70d307ee881
cscinsight <cscinsight.ti@yandex.com>
2018-06-06 23:33:52 -0400

    Initial commit

--- daily.sh
+++ daily.sh
@@ -1,4 +1,5 @@
-cp -u /home/huginn/huginn/public/mydailynews/ /home/cscinsight.github.io
+#cp -u /home/huginn/huginn/public/mydailynews/ /home/cscinsight.github.io
+python /home/test/githubnews.py
 git add --all
 git commit -m "Initial commit"
 git push -u origin master
